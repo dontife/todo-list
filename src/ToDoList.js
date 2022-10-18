53d63303a80641b13ea685999a2ee1ec79b36e4a
dontife <dontife@ymail.com>
2022-10-18 14:54:44 -0400

    update file

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -5,14 +5,14 @@ import ToDo from "./ToDo"
 const ToDoList = ({toDoList, handleToggle, handleFilter}) => {
     return (
         <div>
-           {toDoList.map(todo => {
-               return (
-                   <ToDo todo={todo} handleToggle={handleToggle} handleFilter={handleFilter}/>
-               )
-           })}
-           <button style={{margin: '20px'}} onClick={handleFilter}>Clear Completed</button>
-       </div>
-    )
+            {toDoList.map(todo => {
+                return (
+                    <ToDo todo={todo} handleToggle={handleToggle} handleFilter={handleFilter}/>
+                )
+            })}
+            <button style={{margin: '20px'}} onClick={handleFilter}>Clear Completed</button>
+        </div>
+    );
  
 
 }
