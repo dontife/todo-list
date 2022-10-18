5933702e92bea7b61df0433a898a9a5edd7d3b44
dontife <dontife@ymail.com>
2022-10-18 14:49:15 -0400

    update deleted complete tasks feature work

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import './App.css';
 //components
 import Header from "./Header";
 import ToDoList from "./ToDoList";
+import Form from "./ToDoForm";
 
 
 
 function App() {
   const [toDoList, setToDoList] = useState(data);
+  
   const handleToggle = (id) => {
     let mapped = toDoList.map(task => {
       return task.id == id ? { ...task, complete: !task.complete } : { ...task};
@@ -23,11 +25,17 @@ function App() {
     });
     setToDoList(filtered);
   }
+  const addTask = (userInput) => {
+    let copy = [...toDoList];
+    copy = [...copy, { id: toDoList.length + 1, task: userInput, complete: false }];
+    setToDoList(copy);
+  }
 
   return (
     <div className="App">
       <Header />
-      <ToDoList toDoList={toDoList}/>
+      <ToDoList toDoList={toDoList} handleToggle={handleToggle} handleFilter={handleFilter}/>
+      <Form addTask={addTask}/> 
     </div>
   );
 }
