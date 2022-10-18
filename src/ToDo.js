92e7049931353147f4d336e6c5e38f0ffd50b85f
dontife <dontife@ymail.com>
2022-10-18 14:51:45 -0400

    add the handleToggle props to the ToDo component

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,10 +1,15 @@
 import React from 'react';
  
-const ToDo = ({todo}) => {
+const ToDo = ({todo, handleToggle}) => {
+   const handleClick = (e) => {
+      e.preventDefault()
+      handleToggle(e.currentTarget.id)
+  }
+
    return (
-    <div className={todo.complete ? "strike" : ""}>
-       {todo.task}
-    </div>
+      <div id={todo.id} key={todo.id + todo.task} name="todo" value={todo.id} onClick={handleClick} className={todo.complete ? "todo strike" : "todo"}>
+      {todo.task}
+  </div>
    );
 };
  
